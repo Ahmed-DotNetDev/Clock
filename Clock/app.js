787f9d4a47e1cc812364fb0699955cb3bbebb023
Ahmed.net <Ahmed.net@gmail.com>
2023-04-18 20:51:09 +0200

    Adding some feature

--- Clock/app.js
+++ Clock/app.js
@@ -7,15 +7,25 @@ setInterval(update, 1000);
 function update() {
     let date = new Date();
 
-    lbl.innerHTML = format(date);
+    lbl.innerHTML = formatTime(date);
 
-    function format(date) {
+    function formatTime(date) {
         let hours = date.getHours();
         let minutes = date.getMinutes();
         let seconds = date.getSeconds();
         let amOrpm = hours >= 12 ? "pm" : "am";
         hours = hours % 12 || 12;
+
+        hours = formatZero(hours);
+        minutes = formatZero(minutes);
+        seconds = formatZero(seconds);
+
         return `${hours}:${minutes}:${seconds} ${amOrpm}`;
+    }
+
+    function formatZero(time) {
+        time = time.toString();
+        return time.lenght < 2 ? "0" + time : time;
     }
 }
 
